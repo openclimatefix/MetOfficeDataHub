584cf5158d3bba99cb2b9cace60b48397c8f4a37
peterdudfield <peter.dudfield@hotmail.com>
2022-03-07 14:32:53 +0100

    add verbose error message from api

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ import xarray as xr
 from click.testing import CliRunner
 
 from metofficedatahub.app import run
-from tests.conftest import mocked_requests_get
+from tests.conftest import mocked_requests_get, mocked_requests_get_error
 
 runner = CliRunner()
 
@@ -18,3 +18,15 @@ def test_save_to_zarr(mock_get):
             run, ["--api-key", "fake", "--api-secret", "fake", "--save-dir", tmpdirname]
         )
         assert response.exit_code == 0
+
+
+@mock.patch("requests.get", side_effect=mocked_requests_get_error)
+def test_save_to_zarr(mock_get):
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        response = runner.invoke(
+            run, ["--api-key", "fake", "--api-secret", "fake", "--save-dir", tmpdirname]
+        )
+        assert response.exit_code == 1
+        print(response.exception)
+
