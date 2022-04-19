ace2dc10b736c80917d0c12de985eb04f8bb09a7
peterdudfield <peter.dudfield@hotmail.com>
2022-04-19 08:57:13 +0100

    add test to check fiels arent downloaded again make cache dir be env var, add option that cache dir could be in s3

--- tests/test_multiple_files.py
+++ tests/test_multiple_files.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 from unittest import mock
+from datetime import datetime
 
 import xarray as xr
 
@@ -29,6 +30,28 @@ def test_download_all(mock_get, metofficedatahub):
         assert len(metofficedatahub.files) > 0
         for file in metofficedatahub.files:
             assert os.path.exists(file.local_filename)
+
+
+@mock.patch("requests.get", side_effect=mocked_requests_get)
+def test_download_repeat(mock_get, metofficedatahub):
+    """Check that files are not downloaded again"""
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        metofficedatahub.cache_dir = tmpdirname
+        metofficedatahub.download_all_files(order_ids=["test_order_id"])
+
+        assert len(metofficedatahub.files) > 0
+        for file in metofficedatahub.files:
+            assert os.path.exists(file.local_filename)
+
+        # downloaded for second time
+        datetime_now = datetime.now()
+        metofficedatahub.download_all_files(order_ids=["test_order_id"])
+
+        # make sure the files arent downloaded again
+        for file in metofficedatahub.files:
+            assert os.path.exists(file.local_filename)
+            creation_time = os.path.getmtime(file.local_filename)
+            assert datetime.fromtimestamp(creation_time) < datetime_now
 
 
 @mock.patch("requests.get", side_effect=mocked_requests_get)
