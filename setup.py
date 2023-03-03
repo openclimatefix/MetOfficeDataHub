2af92b925c3a345756ed7049107be95e69728347
BumpVersion Action <bumpversion@github-actions>
2023-03-03 16:49:23 +0000

    Bump version: 1.1.20 → 1.1.21

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="metoffice-weather-datahub",
-    version="1.1.20",
+    version="1.1.21",
     license="MIT",
     description="Python wrapper for the UK Met Office Weather DataHub API",
     author="Peter Dudfield",
