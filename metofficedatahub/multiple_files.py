89e9d2e598da4686dc77cf46526161d1a984b504
peterdudfield <peter.dudfield@hotmail.com>
2023-03-21 09:09:59 +0000

    refactor

--- metofficedatahub/multiple_files.py
+++ metofficedatahub/multiple_files.py
@@ -236,12 +236,16 @@ def _chunk(dataset: xr.Dataset, *, ideal_chunk_size_mb: float) -> xr.Dataset:
     # Number of floats in a megabyte (assuming 64 bit floats).
     num_float_in_mb = 1024 * 1024 / 8
 
-    # becasue of compressions we can make this number larger
-    num_float_in_mb = num_float_in_mb * 5
+    # because of compressions we can make this number larger
+    compression_factor = 5
 
     # Calculate the `size` do we need for x and y if we want to have chunks of
     # `ideal_chunk_size_mb` megabytes.
-    size = int(math.sqrt(ideal_chunk_size_mb * num_float_in_mb / num_step / num_variables))
+    size = int(
+        math.sqrt(
+            ideal_chunk_size_mb * compression_factor * num_float_in_mb / num_step / num_variables
+        )
+    )
 
     return dataset.chunk(dict(init_time=1, step=num_step, variable=num_variables, x=size, y=size))
 
